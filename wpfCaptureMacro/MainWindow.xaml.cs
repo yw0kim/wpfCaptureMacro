c4180f6a7afcff4f6845f88ce550a6dcbca510d2
yw0kim <koain@naver.com>
2019-08-22 20:15:35 +0900

    전체 구조 수정 반복수 입력 까지 완료

--- wpfCaptureMacro/MainWindow.xaml.cs
+++ wpfCaptureMacro/MainWindow.xaml.cs
@@ -20,128 +20,205 @@ namespace wpfCaptureMacro
     /// MainWindow.xaml에 대한 상호 작용 논리
     /// </summary>
     /// 
-    public enum ActionEnum {MouseClick, MouseUnClick, MouseMove, ImgCapture};
+    public enum ActionEnum {OuterIter, OuterNext, InnerIter, InnerNext, ImgCapture, DstPath};
     // 영역 지정
     // 캡처 
     class CAction
     {
-        public CAction(ActionEnum _action, int _order)
+        public CAction(ActionEnum _action)
         {
             action = _action;
-            order = _order;
             isActionSet = 'x';
-            iteration = 0;
+            isTested = 'x';
             switch (action)
             {
-                case ActionEnum.MouseClick:
-                    strAction = "마우스 L클릭";
+                case ActionEnum.OuterIter:
+                    strAction = "화 반복 수 지정";
                     break;
-                case ActionEnum.MouseUnClick:
-                    strAction = "마우스 L클릭해제";
+                case ActionEnum.OuterNext:
+                    strAction = "다음 화 위치 지정";
                     break;
-                case ActionEnum.MouseMove:
-                    strAction = "마우스 이동";
+                case ActionEnum.InnerIter:
+                    strAction = "페이지 최대 반복 수 지정";
+                    break;
+                case ActionEnum.InnerNext:
+                    strAction = "다음 페이지 위치 지정";
                     break;
                 case ActionEnum.ImgCapture:
-                    strAction = "이미지 캡처";
+                    strAction = "이미지 캡처 영역 지정";
                     break;
-                //case ActionEnum.StringInput:
-                //    strAction = " 문자열 입력";
-                //    break;
-                //case ActionEnum.KeboardClick:
-                //    strAction = " 키 입력";
-                //    break;
+                case ActionEnum.DstPath:
+                    strAction = "캡처 파일 저장 경로 입력";
+                    break;
                 default:
                     strAction = "알 수 없는 입력";
                     return;
             }
         }
 
-        public int order { get; set; }
+        //public void setMouseClickAction(MainWindow mainWindow)
+        //{
+            
+        //    mainWindow.swtichSetActionWindow();
+        //    mainWindow.switchMainWindow();
+        //}
+
+        //public void setMouseUnClickAction(MainWindow mainWindow)
+        //{
+        //    mainWindow.swtichSetActionWindow();
+        //    mainWindow.switchMainWindow();
+        //}
+
+        //public void setMouseMoveAction(MainWindow mainWindow)
+        //{
+        //    mainWindow.swtichSetActionWindow();
+        //    mainWindow.switchMainWindow();
+        //}
+
+        //public void setImgCaptureAction(MainWindow mainWindow)
+        //{
+        //    mainWindow.swtichSetActionWindow();
+        //    mainWindow.switchMainWindow();
+        //}
+
         public string strAction { get; set; }
         public char isActionSet { get; set; }
-        public int iteration { get; set; }
+        public char isTested { get; set; }
+        public string strValue { get; set; }
 
         public ActionEnum action;
-        public int x, y; // mouseMove, stringInput Target
-        public string input;
+        public int iterNum; // iterNum
+        public int x, y; // mouseClick Target 
+        public int sx, sy, dx, dy;//imgCapture Target
+        public string path; // imgCapture file dst
        
     }
-       
+
 
     public partial class MainWindow : Window
     {
         private List<CAction> actionList = new List<CAction>();
+        // public SetActionWindow setActionWindow;
+        public SetIterWindow iterWindow;
 
         public MainWindow()
         {
             InitializeComponent();
+            foreach(ActionEnum actionEnum in Enum.GetValues(typeof(ActionEnum)))
+            {
+                CAction tmpAction = new CAction(actionEnum);
+                actionList.Add(tmpAction);
+            }
+            
             ActionListView.ItemsSource = actionList;
         }
 
-        private void ActionAdd_Btn_Click(object sender, RoutedEventArgs e)
+
+        public void switchMainWindow()
         {
-            // MessageBox.Show(ActionMenu.SelectedItem.ToString() + "를 추가했습니다.");
-            CAction tmpAction = new CAction((ActionEnum)ActionMenu.SelectedIndex,
-                                    actionList.Count + 1);
+            App.Current.MainWindow = this;
+            this.Show();
+        }
 
-            actionList.Add(tmpAction);
-
+        void iterGetEvent(object sender, iterNumArg e)
+        {
+            actionList[e.SelectedIdx].iterNum = e.IterNum;
+            actionList[e.SelectedIdx].strValue = "반복수 : " + e.IterNum.ToString();
+            actionList[e.SelectedIdx].isActionSet = 'o';
             ActionListView.Items.Refresh();
         }
 
-        private void ActionDel_Btn_Click(object sender, RoutedEventArgs e)
+        public void switchIterWindow(ActionEnum actionEnum, int selectedIdx)
         {
-            if (ActionListView.SelectedIndex > -1)
+            if (actionEnum == ActionEnum.OuterIter || actionEnum == ActionEnum.InnerIter)
             {
-                int sel = ActionListView.SelectedIndex;
-                actionList.RemoveAt(sel);
-
-                for (int i = sel; i < actionList.Count; i++)
-                {
-                    actionList[i].order = i + 1;
-                }
-                
-                ActionListView.Items.Refresh();
+                iterWindow = new SetIterWindow(actionEnum, selectedIdx);
+                App.Current.MainWindow = iterWindow;
+                iterWindow.goBackMainWindowEvent += new EventHandler<iterNumArg>(iterGetEvent);
+                iterWindow.Show();
             }
         }
 
+
         private void ActionSet_Btn_Click(object sender, RoutedEventArgs e)
         {
+            int sel = ActionListView.SelectedIndex;
+            if (sel > -1)
+            {
+                CAction toSetAction = actionList[sel];
+                ActionEnum actionEnum = toSetAction.action;
+
+                switch (actionEnum)
+                {
+                    case ActionEnum.OuterIter:
+                    case ActionEnum.InnerIter:
+                        switchIterWindow(actionEnum, sel);
+                        break;
+                    case ActionEnum.OuterNext:
+                    case ActionEnum.InnerNext:
+                        break;
+                    case ActionEnum.ImgCapture:
+                        break;
+                    case ActionEnum.DstPath:
+                        break;
+                    default:
+                        return;
+                }
+
+                actionList[sel] = toSetAction;
+            }
 
         }
 
-        private void Simulation_Btn_Click(object sender, RoutedEventArgs e)
+        private void ActionTest_Btn_Click(object sender, RoutedEventArgs e)
         {
 
-            for (int i = 0; i < actionList.Count; i++)
-                MessageBox.Show("액션 : " + actionList[i].action);
-        }
 
+
+        }
         private void Start_Btn_Click(object sender, RoutedEventArgs e)
         {
-            DrawingVisual dv = new DrawingVisual();
-            DrawingContext dc = dv.RenderOpen();
 
 
-            dc.DrawRectangle(new VisualBrush(element), null,
 
-new Rect(new Point(0, 0), new Point(element.ActualWidth, element.ActualHeight)));
-
-       dc.Close();
-
- 
-
-
-    RenderTargetBitmap target =
-
-new RenderTargetBitmap((int)element.ActualWidth, (int)element.ActualHeight,
-
-        96, 96, System.Windows.Media.PixelFormats.Pbgra32);
-
-       
-    target.Render(drawingVisual);
-
-        
         }
+
+        /*
+public void swtichSetActionWindow(ActionEnum action)
+{
+    setActionWindow = new SetActionWindow(action);
+    App.Current.MainWindow = setActionWindow;
+    setActionWindow.Show();
+}
+
+
+private void ActionAdd_Btn_Click(object sender, RoutedEventArgs e)
+{
+    // MessageBox.Show(ActionMenu.SelectedItem.ToString() + "를 추가했습니다.");
+    CAction tmpAction = new CAction((ActionEnum)ActionMenu.SelectedIndex,
+                            actionList.Count + 1);
+
+    actionList.Add(tmpAction);
+
+    ActionListView.Items.Refresh();
+}
+
+private void ActionDel_Btn_Click(object sender, RoutedEventArgs e)
+{
+    int sel = ActionListView.SelectedIndex;
+
+    if (sel > -1)
+    {
+        actionList.RemoveAt(sel);
+
+        for (int i = sel; i < actionList.Count; i++)
+        {
+            actionList[i].order = i + 1;
+        }
+
+        ActionListView.Items.Refresh();
+    }
+}
+*/
+    }
 }
---
+++ wpfCaptureMacro/SetIterWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace wpfCaptureMacro
+{
+    /// <summary>
+    /// SetIterWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    /// http://blog.naver.com/PostView.nhn?blogId=ronghuan&logNo=110031908726 // window간 데이터
+    /// 
+
+    public partial class SetIterWindow : Window
+    {
+        public event EventHandler<iterNumArg> goBackMainWindowEvent;
+        public int selectedIdx;
+        public ActionEnum actionEnum;
+
+        public SetIterWindow(ActionEnum _actionEnum, int _selectedIdx)
+        {
+            InitializeComponent();
+            actionEnum = _actionEnum;
+            if (actionEnum == ActionEnum.OuterIter)
+            {
+                IterTextBlock.Text = "총 몇 화인지 입력";
+            } else if (actionEnum == ActionEnum.InnerIter)
+            {
+                IterTextBlock.Text = "최대 몇 페이지인지 입력";
+            }
+            selectedIdx = _selectedIdx;
+        }
+
+        private void inputBtn_Click(object sender, RoutedEventArgs e)
+        {
+            int input;
+            try
+            {
+                 input = Int32.Parse(IterTextBox.Text);
+
+            }
+            catch (FormatException)
+            {
+                IterTextBlock.Text += "\n 입력 오류 재입력.";
+                return; 
+            }
+            goBackMainWindowEvent(this, new iterNumArg(input, this.selectedIdx));
+            this.Close();
+        }
+
+        private void IterTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+
+    public class iterNumArg : EventArgs
+    {
+        public iterNumArg(int _iterNum, int _selectedIdx)
+        {
+            iterNum = _iterNum;
+            selectedIdx = _selectedIdx;
+        }
+        private int iterNum;
+        public int IterNum
+        {
+            get { return iterNum; }
+        }
+
+        private int selectedIdx;
+        public int SelectedIdx
+        {
+            get { return selectedIdx; }
+        }
+    }
+}
